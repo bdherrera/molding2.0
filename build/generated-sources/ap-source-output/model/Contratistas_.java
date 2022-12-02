c3653e2a89e11989d9275563c0885b9138bbe856
bdherrera <braherrera2003@hotmail.com>
2022-12-01 23:29:26 -0500

    modulo contratistas completado

---
+++ build/generated-sources/ap-source-output/model/Contratistas_.java
@@ -0,0 +1,20 @@
+package model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-12-01T22:52:33")
+@StaticMetamodel(Contratistas.class)
+public class Contratistas_ { 
+
+    public static volatile SingularAttribute<Contratistas, Integer> rut;
+    public static volatile SingularAttribute<Contratistas, Integer> idcontratistas;
+    public static volatile SingularAttribute<Contratistas, String> direccionObra;
+    public static volatile SingularAttribute<Contratistas, String> valorContrato;
+    public static volatile SingularAttribute<Contratistas, String> nombreContratista;
+    public static volatile SingularAttribute<Contratistas, String> fechaContrato;
+    public static volatile SingularAttribute<Contratistas, String> correo;
+    public static volatile SingularAttribute<Contratistas, String> telefono;
+
+}
